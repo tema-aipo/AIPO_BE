c74cd90744ad13efc6445e848274c0102b560a84
michael <michael830@gachon.ac.kr>
2026-04-05 00:41:38 +0900

    feat: initial backend setup

---
+++ src/test/java/com/aipo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
